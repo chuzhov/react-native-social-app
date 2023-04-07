3c28591e98e12d811a6ed42d854bfe21bc3a1d43
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-04-07 15:20:59 +0300

    split to App and MainNavigator

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 
-import LoginScreen from './Screens/LoginScreen';
-import RegisterScreen from './Screens/RegistrationScreen';
-import Home from './Screens/Home';
-import MapScreen from './Screens/MapScreen';
-import CommentsScreen from './Screens/CommentsScreen';
-import GoBackBtn from './Components/GoBackBtn';
 import store from './redux/store';
 
-export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const appFonts = {
-    'Roboto-Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
-    'Roboto-Medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
-    'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf'),
-  };
+import MainNavigator from './MainNavigator';
 
+const appFonts = {
+  'Roboto-Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
+  'Roboto-Medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
+  'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf'),
+};
+
+export default () => {
+  const [appIsReady, setAppIsReady] = useState(false);
   useEffect(() => {
     async function prepare() {
       try {
@@ -38,48 +32,12 @@ export default function App() {
     prepare();
   }, []);
 
-  const Stack = createStackNavigator();
-
   if (!appIsReady) {
     return null;
   } else
     return (
       <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Registration"
-              component={RegisterScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Home"
-              component={Home}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Map"
-              component={MapScreen}
-              options={({ navigation }) => ({
-                headerLeft: () => <GoBackBtn navigation={navigation} />,
-                headerShown: true,
-              })}
-            />
-            <Stack.Screen
-              name="Comments"
-              component={CommentsScreen}
-              options={({ navigation }) => ({
-                headerLeft: () => <GoBackBtn navigation={navigation} />,
-                headerShown: true,
-              })}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <MainNavigator />
       </Provider>
     );
-}
+};
---
+++ MainNavigator.js
@@ -0,0 +1,55 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegistrationScreen';
+import Home from './Screens/Home';
+import MapScreen from './Screens/MapScreen';
+import CommentsScreen from './Screens/CommentsScreen';
+import GoBackBtn from './Components/GoBackBtn';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { isLoggedInSelector } from './redux/auth/authSelectors';
+import { useSelector } from 'react-redux';
+
+const MainNavigator = () => {
+  const Stack = createStackNavigator();
+  const isAuthenticated = useSelector(isLoggedInSelector);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={({ navigation }) => ({
+          headerLeft: () => <GoBackBtn navigation={navigation} />,
+          headerShown: true,
+        })}
+      >
+        {isAuthenticated ? (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Map" component={MapScreen} />
+            <Stack.Screen name="Comments" component={CommentsScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Registration"
+              component={RegisterScreen}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default MainNavigator;
